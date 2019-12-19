2bb9f67b2fcba18547e71085d61974fa81450085
twobear <gx.lai@easycon.cn>
2019-12-19 18:35:10 +0800

    The first commit

---
+++ .fatherrc.ts
@@ -0,0 +1,29 @@
+import {IBundleOptions} from 'father'
+
+// @ts-ignore
+const options: IBundleOptions = {
+    // entry: 'src/index.tsx',
+    file: 'beast',
+    esm: {
+        type: 'rollup'
+    },
+    cjs: 'rollup',
+    // @ts-ignore
+    doc: {
+        // themeConfig: { mode: 'dark' },
+        base: '/beast-lib',
+        // @ts-ignore
+        version: '1.2.0'
+    },
+    // 是否把 helper 方法提取到 @babel/runtime 里。只对 esm 有效。官网：https://babeljs.io/docs/en/babel-runtime
+    runtimeHelpers: true,
+    extraBabelPlugins: [ // 配置 babel-plugin-import 按需加载 antd
+        ['babel-plugin-import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+        }],
+    ],
+};
+
+export default options;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+// import React from "react";
+export {default} from './components/Button'
+
+// export default function () {
+//     return (
+//         <Button type={'primary'} >sdf</Button>
+//     )
+// }
