0ada5643391ac25bcf0127f82922156e0114f0af
twobear <gx.lai@easycon.cn>
2019-12-19 20:21:27 +0800

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-export {default} from './components/Button'
-
-// export default function () {
-//     return (
-//         <Button type={'primary'} >sdf</Button>
-//     )
-// }
