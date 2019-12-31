7f4d5d97298776db5472ef7f60b68d2e1ddf96b1
laixstx <18826408945@163.com>
2019-12-31 15:32:44 +0800

    调整脚本目录

--- index.tsx
+++ index.tsx
@@ -1,3 +1,1 @@
 export {default as BtButton} from './src/components/BtButton';
-export {default as BtIcon} from './src/components/BtIcon';
-export {default as BtTypography} from './src/components/BtTypography';
--- scripts/doc_deploy.sh
+++ scripts/doc_deploy.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 yarn doc:build
 yarn doc:deploy
